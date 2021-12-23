c71c60289d95f3accea208609a2de1b066ec528d
Pandurang Gokul Pailvan <PGokul.Pailvan@wolterskluwer.com>
2021-12-23 19:32:24 +0530

    Updated logfile Name

--- SBMMember.Web/Startup.cs
+++ SBMMember.Web/Startup.cs
@@ -33,7 +33,7 @@ namespace SBMMember.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddFile("Logs/SBMRestAPI-{Date}.txt");
+            loggerFactory.AddFile("Logs/SBMMember-{Date}.txt");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
