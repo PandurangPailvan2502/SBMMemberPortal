a50670cd912899fe01e503f7042c7507f3a66a44
Pandurang Gokul Pailvan <PGokul.Pailvan@wolterskluwer.com>
2022-03-10 18:47:55 +0530

    Splash screen and URL changes done

---
+++ SBMMember.Web/Controllers/SplashScreenController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBMMember.Web.Controllers
+{
+    public class SplashScreenController : Controller
+    {
+        public IActionResult SplashScreen()
+        {
+            return View();
+        }
+    }
+}
