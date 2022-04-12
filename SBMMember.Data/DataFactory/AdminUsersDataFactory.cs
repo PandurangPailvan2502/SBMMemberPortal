62b38843296f1832208ff074824735c685170b59
Pandurang Gokul Pailvan <PGokul.Pailvan@wolterskluwer.com>
2022-04-12 23:50:41 +0530

    Added some methods in AdminUserDataFactory

--- SBMMember.Data/DataFactory/AdminUsersDataFactory.cs
+++ SBMMember.Data/DataFactory/AdminUsersDataFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using SBMMember.Models;
 
 namespace SBMMember.Data.DataFactory
@@ -10,9 +11,11 @@ namespace SBMMember.Data.DataFactory
    public class AdminUsersDataFactory: IAdminUsersDataFactory
     {
         private readonly SBMMemberDBContext adminDBContext;
-        public AdminUsersDataFactory(SBMMemberDBContext dBContext)
+        private readonly ILogger Logger;
+        public AdminUsersDataFactory(SBMMemberDBContext dBContext, ILogger _Logger)
         {
             adminDBContext = dBContext;
+            Logger = _Logger;
         }
 
         public AdminUsers GetAdminUserByMobile(string mobile )
@@ -28,6 +31,77 @@ namespace SBMMember.Data.DataFactory
         {
             return adminDBContext.AdminUsers.Where(x => x.Id == Id && x.Status == "Active").FirstOrDefault();
         }
+
+        public ResponseDTO AddSuperUserDetails(AdminUsers admin)
+        {
+            ResponseDTO responseDTO = new ResponseDTO();
+            try
+            {
+                int affectedRows = 0;
+                admin.Status = "Active";
+                admin.Createdate = DateTime.Now;
+                adminDBContext.AdminUsers.Add(admin);
+                affectedRows = adminDBContext.SaveChanges();
+                if (affectedRows > 0)
+                {
+                    responseDTO = new ResponseDTO()
+                    {
+                        Result = "Success",
+                        Message = "Member Admin User details saved successfully."
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Logger.LogError($"Error occured while adding Admin User details. Exception:{ex.Message}");
+                responseDTO = new ResponseDTO()
+                {
+                    Result = "Failed",
+                    Message = "Admin User details save operation failed."
+                };
+            }
+
+            return responseDTO;
+        }
+
+        public ResponseDTO UpdateDetails(AdminUsers admin)
+        {
+            ResponseDTO responseDTO = new ResponseDTO();
+            try
+            {
+                var adminUser = adminDBContext.AdminUsers.Where(x => x.Id == admin.Id && x.Status == "Active").FirstOrDefault();
+                adminUser.FirstName = admin.FirstName;
+                adminUser.LastName = admin.LastName;
+                adminUser.MobileNo = admin.MobileNo;
+                adminUser.Password = admin.Password;
+                
+
+                int affectedRows = 0;
+
+                affectedRows = adminDBContext.SaveChanges();
+                if (affectedRows > 0)
+                {
+                    responseDTO = new ResponseDTO()
+                    {
+                        Result = "Success",
+                        Message = "Admin user details updated successfully."
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Logger.LogError($"Error occured while updating admin user details. Exception:{ex.Message}");
+                responseDTO = new ResponseDTO()
+                {
+                    Result = "Failed",
+                    Message = "Admin user details save operation failed."
+                };
+            }
+
+            return responseDTO;
+        }
     }
 
     public interface IAdminUsersDataFactory
@@ -35,5 +109,8 @@ namespace SBMMember.Data.DataFactory
         AdminUsers GetAdminUserByMobile(string mobile);
         List<AdminUsers> GetAdminUsers();
         AdminUsers GetUserById(int Id);
+
+        ResponseDTO AddSuperUserDetails(AdminUsers admin);
+        ResponseDTO UpdateDetails(AdminUsers admin);
     }
 }
